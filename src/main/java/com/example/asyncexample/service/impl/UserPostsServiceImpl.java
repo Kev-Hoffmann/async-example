7c613bb5e1f4adab96a88b45176d908d4d8396b8
kevhoffmann <k.hoffmann097@gmail.com>
2023-08-21 13:32:17 +0200

    Add logic to get user and posts from JSONPlaceholder API and merge responses into single object

--- src/main/java/com/example/asyncexample/service/impl/UserPostsServiceImpl.java
+++ src/main/java/com/example/asyncexample/service/impl/UserPostsServiceImpl.java
@@ -1,14 +1,21 @@
 package com.example.asyncexample.service.impl;
 
+import com.example.asyncexample.api.JSONPlaceholderAPI;
 import com.example.asyncexample.dto.UserPostsDto;
 import com.example.asyncexample.service.UserPostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
+@RequiredArgsConstructor
 public class UserPostsServiceImpl implements UserPostsService {
-    @Override
-    public Mono<UserPostsDto> getUserAndPosts(Integer id) {
-    return null;
-    }
+
+  private final JSONPlaceholderAPI jsonPlaceholderAPI;
+
+  @Override
+  public Mono<UserPostsDto> getUserAndPosts(Integer id) {
+    return Mono.zip(jsonPlaceholderAPI.getUser(id), jsonPlaceholderAPI.getPosts(id).collectList())
+        .map(userAndPosts -> new UserPostsDto(userAndPosts.getT1(), userAndPosts.getT2()));
+  }
 }
