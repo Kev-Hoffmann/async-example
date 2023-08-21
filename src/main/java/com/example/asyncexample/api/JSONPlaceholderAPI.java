680911d8bb79125fb3f8f4ac3f91e94a9e7b015c
kevhoffmann <k.hoffmann097@gmail.com>
2023-08-21 13:26:04 +0200

    Throw ResourceNotFoundException if JSONPlaceholder API returns 404 Not Found

--- src/main/java/com/example/asyncexample/api/JSONPlaceholderAPI.java
+++ src/main/java/com/example/asyncexample/api/JSONPlaceholderAPI.java
@@ -2,11 +2,14 @@ package com.example.asyncexample.api;
 
 import com.example.asyncexample.dto.PostDto;
 import com.example.asyncexample.dto.UserDto;
+import com.example.asyncexample.error.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Component
 @RequiredArgsConstructor
@@ -15,7 +18,15 @@ public class JSONPlaceholderAPI {
   private final WebClient webClient;
 
   public Mono<UserDto> getUser(Integer id) {
-    return webClient.get().uri("/users/" + id).retrieve().bodyToMono(UserDto.class);
+    return webClient
+        .get()
+        .uri("/users/" + id)
+        .retrieve()
+        .onStatus(
+            NOT_FOUND::equals,
+            clientResponse ->
+                Mono.error(new ResourceNotFoundException("User " + id + " not found!")))
+        .bodyToMono(UserDto.class);
   }
 
   public Flux<PostDto> getPosts(Integer userId) {
